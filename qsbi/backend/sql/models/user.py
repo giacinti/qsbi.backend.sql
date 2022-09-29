613057c6169315a2a085ff6086ca8afafffa5764
giacinti <philippe@giacinti.com>
2022-09-29 15:48:59 +0200

    add scopes in user model + attribute method for serialization

--- qsbi/backend/sql/models/user.py
+++ qsbi/backend/sql/models/user.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import json
 from sqlalchemy import Column, Float, Integer, Text, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
@@ -11,6 +12,18 @@ class User(Base):
     lastname = Column(Text)
     password = Column(Text)
     active = Column(Integer)
+    scopes_str = Column(Text)
     notes = Column(Text)
+
+    @property
+    def scopes(self):
+        lst = []
+        if self.scopes_str:
+            lst = json.loads(self.scopes_str)
+        return lst
+
+    @scopes.setter
+    def scopes(self, lst):
+        self.scopes_str = json.dumps(lst)
 
     logs = relationship("AuditLog", back_populates="user")
